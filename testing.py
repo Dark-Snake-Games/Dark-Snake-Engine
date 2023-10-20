ff85f4a779a99c097c3a26c9025b2b196e07b69d
R9_ <neamtrares@naver.com>
2023-10-20 13:15:26 +0300

    Delta and set FPS

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ from DSEngine import *
 from pygame import Vector2, Vector3
 from pygame.display import update
 
-window = Window(size=(1280, 720))
+window = Window(fps=60, size=(1280, 720))
 type2d = Type2D("GUI")
 rect = Rect2D(position=Vector2(150, 55), size=Vector2(5, 5))
 image = Image2D(filename="Test.png", position=Vector2(150, 55))
@@ -15,6 +15,6 @@ while window.running:
         music.play()
     keys = window.frame()
     acc = Vector2(0.0, 0.0)
-    acc.x = keys[key_to_scancode("d")]-keys[key_to_scancode("a")]
-    acc.y = keys[key_to_scancode("s")]-keys[key_to_scancode("w")]
+    acc.x = (keys[key_to_scancode("d")]-keys[key_to_scancode("a")])*window.delta
+    acc.y = (keys[key_to_scancode("s")]-keys[key_to_scancode("w")])*window.delta
     image.move(acc)
