4882e4fa033ada05ec7696607e5e45aff949c27c
ThatBoiDev <mart.zielman@kpnmail.nl>
2023-10-21 17:54:08 +0200

    Bug fix and advanced init message

--- DSEngine/__init__.py
+++ DSEngine/__init__.py
@@ -1,7 +1,22 @@
+__version__ = "0.0.1-alpha"
+__license__ = "MIT"
+
+import os
+import sys
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame as pg
 from .etypes import *
 from .animated import *
 from .gui import *
 
 pg.init()
-print("Hello from the Dark Snake Team")
+
+print(
+  f"Dark Snake Engine {__version__}",
+  f"python {sys.version.split()[0]}  pygame-ce {pg.version.ver}",
+  f"© 2023 Dark Snake Games, GitHub contributors,",
+  f"licensed under {__license__}",
+  sep="\n",
+  end="\n\n"
+)
+print("*** DEBUG INFORMATION ***")
