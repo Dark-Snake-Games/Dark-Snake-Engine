d18132b23f18990ac3e2c91400f73663e553a45a
R9_ <neamtrares@naver.com>
2023-10-23 16:57:59 +0300

    Button hover and press

--- DSEngine/gui.py
+++ DSEngine/gui.py
@@ -8,9 +8,11 @@ class Button(Rect2D):
         self.debug = False
         self.layer = layer
         self.position = position
+        self.pressed = False
+        self.hovered = False
         #self.rect = pygame.Rect(position.x, position.y, position.x+size.x, position.x+size.y)
         self.text = text
-        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.font = pygame.font.SysFont('Roboto', 30)
         self.text_surface = self.font.render(self.text, False, (255, 255, 255))
         self.color_rect = self.text_surface.get_rect()
         #self.rect = self.text_surface.get_rect()
@@ -18,7 +20,15 @@ class Button(Rect2D):
         #print("Initialized super()")
     
     def render(self, window: Window):
-        #super().render(window)
+        if self.color_rect.collidepoint(window.get_mouse_pos()):
+            self.hovered = True
+            leftclick, _, _ = pygame.mouse.get_pressed()
+            if leftclick:
+                self.pressed = True
+            else:
+                self.pressed = False
+        else:
+            self.hovered = False
         pygame.draw.rect(window.surface, (0, 0, 0), self.color_rect)
         window.surface.blit(self.text_surface, (self.position.x, self.position.y))
         #print("Sprite2D render done")
--- gui_test.py
+++ gui_test.py
@@ -2,8 +2,15 @@ from DSEngine import *
 from pygame import Vector2
 from pygame.display import update
 
-window = Window(fps=120, size=(1280, 720), bg=(255, 255, 255))
-button = Button(text="Press me!", position=Vector2(0, 0), size=Vector2(500, 100))
+old_pressed = False
+window = Window(fps=120, size=(1280, 720))
+button = Button(text="Press me!", position=Vector2(0, 0))
 button.init(window)
 while window.running:
     keys = window.frame()
+    if button.pressed != old_pressed:
+        if button.pressed:
+            print("Pressed")
+        else:
+            print("Not pressed")
+        old_pressed = button.pressed
