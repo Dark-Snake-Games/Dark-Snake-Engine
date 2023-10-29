d800d418525ec6cb6323a7bd41043f354e6bfff8
R9_ <neamtrares@naver.com>
2023-10-29 17:39:37 +0200

    SCP-999 as a classs

--- game.py
+++ game.py
@@ -3,7 +3,25 @@ from pygame import Vector2
 from pygame.display import update
 from sys import exit
 from random import randint
+
+from DSEngine.animated import AnimationSheet
+from DSEngine.etypes import pygame
 default_title="Project: SCP"
+
+class SCP999(AnimatedSprite2D):
+    def __init__(self, position=...):
+        sheet = AnimationSheet(default=Image2D("Test.png"))
+        self.last_secs = 0
+        super().__init__(sheet, 1, position)
+    
+    def render(self, window: Window):
+        if int(window.seconds-self.last_secs) >= 20:
+            rx = randint(0, window.size[0])
+            ry = randint(0, window.size[1])
+            pos = Vector2(rx, ry)
+            self.move_towards(pos)
+            self.last_secs = int(window.seconds)
+        super().render(window)
 
 def main_menu():
     global default_title
@@ -32,21 +50,13 @@ def main_menu():
 def scp_999_scene():
     global default_title
     window = Window(title=default_title, fps=120, size=(1280, 720), bg=(100, 100, 100))
-    last_secs = 0
     audio_man = AudioManager()
     text = Text2D("SCP-999", position=Vector2(550, 0))
-    animations = AnimationSheet(default=Image2D("Test.png"))
-    scp = AnimatedSprite2D(animations, position=Vector2(640, 360))
+    scp = SCP999(position=Vector2(640, 360))
     text.init(window)
     scp.init(window)
     scp.move_towards(Vector2(0, 0))
     while window.running:
-        if int(window.seconds-last_secs) >= 10:
-            rx = randint(0, window.size[0])
-            ry = randint(0, window.size[1])
-            pos = Vector2(rx, ry)
-            scp.move_towards(pos)
-            last_secs = int(window.seconds)
         keys = window.frame()
         if keys[27]:
             return 1
